c699894a40340798b1d746ab4040afb509053182
dpandit24 <dpandit24@rku.ac.in>
2025-10-03 00:24:08 +0530

    feat(case): 9th case - add test to ensure that the code can handle multiple delimiters with length longer than one char

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -210,6 +210,61 @@ describe('Calculator', () => {
       expect(calculator.add('//[abc][xyz][del]\n1abc2xyz3del4')).toBe(10);
     });
 
+    test('should handle multiple delimiters with length > 1 character', () => {
+      expect(calculator.add('//[abc][def]\n1abc2def3')).toBe(6);
+      expect(calculator.add('//[abc][def]\n1abc2def3')).toBe(6);
+      expect(calculator.add('//[hello][world]\n1hello2world3')).toBe(6);
+      expect(calculator.add('//[test][case]\n1test2case3')).toBe(6);
+      expect(calculator.add('//[verylong][delimiter]\n1verylong2delimiter3')).toBe(6);
+    });
+
+    test('should handle multiple delimiters with special characters and length > 1', () => {
+      expect(calculator.add('//[***][###]\n1***2###3')).toBe(6);
+      expect(calculator.add('//[$$$][@@@]\n1$$$2@@@3')).toBe(6);
+      expect(calculator.add('//[---][+++]\n1---2+++3')).toBe(6);
+      expect(calculator.add('//[!!!][???]\n1!!!2???3')).toBe(6);
+      expect(calculator.add('//[***][xyz][###]\n1***2xyz3###4')).toBe(10);
+    });
+
+    test('should handle multiple delimiters with mixed lengths', () => {
+      expect(calculator.add('//[a][abc][def]\n1a2abc3def4')).toBe(10);
+      expect(calculator.add('//[abc][d][efg]\n1abc2d3efg4')).toBe(10);
+      expect(calculator.add('//[x][y][z][abc]\n1x2y3z4abc5')).toBe(15);
+      expect(calculator.add('//[verylong][a][short]\n1verylong2a3short4')).toBe(10);
+    });
+
+    test('should handle multiple delimiters with numbers and special chars', () => {
+      expect(calculator.add('//[abc][def]\n1abc2def3')).toBe(6);
+      expect(calculator.add('//[abc][xyz][def]\n1abc2xyz3def4')).toBe(10);
+      expect(calculator.add('//[test][case][def]\n1test2case3def4')).toBe(10);
+      expect(calculator.add('//[***][xyz][###]\n1***2xyz3###4')).toBe(10);
+    });
+
+    test('should handle multiple delimiters with spaces and length > 1', () => {
+      expect(calculator.add('//[abc][def]\n1 abc 2 def 3')).toBe(6);
+      expect(calculator.add('//[abc][def]\n 1 abc 2 def 3 ')).toBe(6);
+      expect(calculator.add('//[hello][world]\n1 hello 2 world 3')).toBe(6);
+    });
+
+    test('should handle multiple delimiters with newlines and length > 1', () => {
+      expect(calculator.add('//[abc][def]\n1abc2\n3')).toBe(6);
+      expect(calculator.add('//[abc][def]\n1abc2\n3def4')).toBe(10);
+      expect(calculator.add('//[hello][world]\n1hello2\n3world4')).toBe(10);
+    });
+
+    test('should handle multiple delimiters with negative numbers and length > 1', () => {
+      expect(() => calculator.add('//[abc][def]\n-1abc2def3')).toThrow('negatives not allowed - -1');
+      expect(() => calculator.add('//[abc][def]\n1abc-2def3')).toThrow('negatives not allowed - -2');
+      expect(() => calculator.add('//[hello][world]\n-1hello-2world-3')).toThrow('negatives not allowed - -1, -2, -3');
+    });
+
+    test('should handle multiple delimiters with numbers > 1000 and length > 1', () => {
+      expect(calculator.add('//[abc][def]\n2abc1001def3')).toBe(5);
+      expect(calculator.add('//[abc][def]\n1abc1001def3')).toBe(4);
+      expect(calculator.add('//[hello][world]\n1001hello1002world1003')).toBe(0);
+      expect(calculator.add('//[test][case]\n1000test1001case1002')).toBe(1000);
+    });
+
     test('should handle custom delimiters with spaces', () => {
       expect(calculator.add('//;\n1 ; 2')).toBe(3);
       expect(calculator.add('//|\n 1 | 2 | 3 ')).toBe(6);
